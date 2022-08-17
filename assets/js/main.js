423d94aaecc8e938bef0a1a8aff773985f75f823
Jordan Veloso <jrveloso96@gmail.com>
2022-08-17 14:20:33 -0500

    Added scroll to reveal animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,23 @@
-/*
-	Telemetry by Pixelarity
-	pixelarity.com | hello@pixelarity.com
-	License: pixelarity.com/license
-*/
+//new
+window.addEventListener('scroll', reveal)
+
+function reveal(){
+	let reveals = document.querySelectorAll('.reveal')
+
+	for(let i = 0; i < reveals.length; i++) {
+		let windowheight = window.innerHeight
+		let revealtop = reveals[i].getBoundingClientRect().top
+		let revealpoint = 150
+
+		if(revealtop < windowheight - revealpoint) {
+			reveals[i].classList.add('active')
+		} else {
+			reveals[i].classList.remove('active')
+		}
+	}
+}
+
+
 
 (function($) {
 
